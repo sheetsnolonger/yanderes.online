fa3eef469cb6ed10d59db2283706bd99647ebfed
sheetsnolonger <sam.personalt@gmail.com>
2026-05-23 18:59:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(session({
-  secret: process.env.SESSION_SECRET || "change-this-secret",
+  secret: process.env.SESSION_SECRET || "deso;fjuh23-0r83",
   resave: false,
   saveUninitialized: false
 }));
